fa4a6fe4601b9f0af4f976b17d512c9b13e2db52
Luis Baroni <baroniluishenrique@gmail.com>
2024-08-05 22:43:35 -0300

    feat: add blockchain dependencies to backend

--- validator-backend/src/app.controller.ts
+++ validator-backend/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ethers } from 'ethers';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const provider = new ethers.JsonRpcProvider('http://0.0.0.0:8545');
+
+    const account = await provider.listAccounts();
+
+    const balance = provider.getBalance(account[1].address);
+    return (await balance).toString();
   }
 }
