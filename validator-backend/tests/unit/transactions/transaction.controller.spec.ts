f2c1de1feef564d24e9d5a19a051406c3cc5c663
Luis Baroni <baroniluishenrique@gmail.com>
2024-08-08 21:09:27 -0300

    test: organizes and updates unit tests

---
+++ validator-backend/tests/unit/transactions/transaction.controller.spec.ts
@@ -0,0 +1,72 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { TransactionController } from '../../../src/transactions/transaction.controller'
+import { TransactionService } from '../../../src/transactions/transaction.service'
+
+describe('TransactionController', () => {
+  let transactionController: TransactionController
+  let transactionService: TransactionService
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [TransactionController],
+      providers: [
+        {
+          provide: TransactionService,
+          useValue: {
+            buildContractTransaction: jest.fn(),
+            broadcastContractTransaction: jest.fn(),
+          },
+        },
+      ],
+    }).compile()
+
+    transactionController = module.get<TransactionController>(
+      TransactionController,
+    )
+    transactionService = module.get<TransactionService>(TransactionService)
+  })
+
+  it('should be defined', () => {
+    expect(transactionController).toBeDefined()
+  })
+
+  describe('buildContractTransaction', () => {
+    it('should build the transaction', async () => {
+      const mockSignedTransaction = 'mockedSignedTransaction'
+
+      transactionService.buildContractTransaction = jest
+        .fn()
+        .mockResolvedValue(mockSignedTransaction)
+
+      const result = await transactionController.buildContractTransaction({
+        publicKey: 'mockedPublicKey',
+        documentHash: 'mockedDocumentHash',
+        contractMethod: 'storeHash',
+      })
+      expect(result).toBe(mockSignedTransaction)
+      expect(transactionService.buildContractTransaction).toHaveBeenCalledWith(
+        'mockedPublicKey',
+        'mockedDocumentHash',
+        'storeHash',
+      )
+    })
+  })
+
+  describe('broadcastContractTransaction', () => {
+    it('should broadcast the transaction', async () => {
+      const mockBroadcastResult = 'mockedBroadcastResult'
+
+      transactionService.broadcastContractTransaction = jest
+        .fn()
+        .mockResolvedValue(mockBroadcastResult)
+
+      const result = await transactionController.broadcastContractTransaction({
+        transaction: 'mockedTransaction',
+      })
+      expect(result).toBe(mockBroadcastResult)
+      expect(
+        transactionService.broadcastContractTransaction,
+      ).toHaveBeenCalledWith('mockedTransaction')
+    })
+  })
+})
---
+++ validator-backend/tests/unit/transactions/transaction.service.spec.ts
@@ -0,0 +1,137 @@
+import { Test, TestingModule } from '@nestjs/testing'
+
+import { TransactionService } from '../../../src/transactions/transaction.service'
+import { InternalServerErrorException } from '@nestjs/common'
+import { ethers } from 'ethers'
+import { ConfigService } from '@nestjs/config'
+
+const mockProvider = {
+  broadcastTransaction: jest.fn(),
+}
+
+const mockWallet = {
+  signingKey: { publicKey: 'mockedPublicKey' },
+  populateTransaction: jest.fn(),
+  signTransaction: jest.fn(),
+}
+
+const mockContract = {
+  storeHash: {
+    populateTransaction: jest.fn(),
+  },
+}
+
+describe('TransactionService', () => {
+  let transactionService: TransactionService
+  let configService: ConfigService
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [],
+      providers: [
+        TransactionService,
+        ConfigService,
+        { provide: 'ETHERS_PROVIDER', useValue: mockProvider },
+        { provide: 'ETHERS_WALLET', useValue: mockWallet },
+        { provide: 'ETHERS_CONTRACT', useValue: mockContract },
+      ],
+    }).compile()
+
+    transactionService = app.get<TransactionService>(TransactionService)
+    configService = app.get<ConfigService>(ConfigService)
+  })
+
+  describe('buildContractTransaction', () => {
+    const mockPublicKey =
+      '0x048318535b54105d4a7aae60c08fc45f9687181b4fdfc625bd1a753fa7397fed753547f11ca8696646f2f3acb08e31016afac23e630c5d11f59f61fef57b0d2aa5'
+    const mockDocumentHash =
+      '0x1234567890123455589012345678901234567890123456789012345678901234'
+    const validMethods = ['storeHash']
+    const contractMethod = 'storeHash'
+
+    it('should return signed transaction of a valid module', async () => {
+      const mockTransaction = { mock: 'transaction' }
+      const mockSignedTransaction = 'mockedSignedTransaction'
+
+      jest.spyOn(configService, 'get').mockReturnValue(validMethods)
+
+      mockContract.storeHash.populateTransaction.mockResolvedValue(
+        mockTransaction,
+      )
+      mockWallet.populateTransaction.mockResolvedValue(mockTransaction)
+      mockWallet.signTransaction.mockResolvedValue(mockSignedTransaction)
+
+      const result = await transactionService.buildContractTransaction(
+        mockPublicKey,
+        mockDocumentHash,
+        contractMethod,
+      )
+      expect(result).toBe(mockSignedTransaction)
+      expect(configService.get).toHaveBeenCalledWith('contract.methods')
+
+      expect(mockContract.storeHash.populateTransaction).toHaveBeenCalledWith(
+        mockDocumentHash,
+        ethers.keccak256(mockPublicKey),
+      )
+      expect(mockWallet.populateTransaction).toHaveBeenCalledWith(
+        mockTransaction,
+      )
+      expect(mockWallet.signTransaction).toHaveBeenCalledWith(mockTransaction)
+    })
+
+    it('should throw InternalServerErrorException on error', async () => {
+      jest.spyOn(configService, 'get').mockReturnValue(validMethods)
+
+      mockContract.storeHash.populateTransaction.mockRejectedValue(
+        new Error('Mocked Error'),
+      )
+
+      await expect(
+        transactionService.buildContractTransaction(
+          mockPublicKey,
+          mockDocumentHash,
+          contractMethod,
+        ),
+      ).rejects.toThrow(InternalServerErrorException)
+    })
+
+    it('should throw BadRequestException on invalid contract method', async () => {
+      jest.spyOn(configService, 'get').mockReturnValue(validMethods)
+
+      await expect(
+        transactionService.buildContractTransaction(
+          mockPublicKey,
+          mockDocumentHash,
+          'invalidMethod',
+        ),
+      ).rejects.toThrow('Invalid contract method')
+    })
+  })
+
+  describe('broadcastContractTransaction', () => {
+    it('should broadcast the transaction', async () => {
+      const mockBroadcastResult = 'mockedBroadcastResult'
+
+      mockProvider.broadcastTransaction.mockResolvedValue(mockBroadcastResult)
+
+      const result =
+        await transactionService.broadcastContractTransaction(
+          'mockedTransaction',
+        )
+      expect(result).toBe(mockBroadcastResult)
+      expect(mockProvider.broadcastTransaction).toHaveBeenCalledWith(
+        'mockedTransaction',
+      )
+    })
+
+    it('should throw InternalServerErrorException on error', async () => {
+      mockProvider.broadcastTransaction.mockRejectedValue(
+        new Error('Mocked Error'),
+      )
+
+      await expect(
+        transactionService.broadcastContractTransaction('mockedTransaction'),
+      ).rejects.toThrow(InternalServerErrorException)
+    })
+  })
+})
