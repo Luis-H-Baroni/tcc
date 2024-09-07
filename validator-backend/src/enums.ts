2b51289c78cbeb4a69bc00fd6b991c60fb9a113e
Luis Baroni <baroniluishenrique@gmail.com>
2024-09-07 10:34:00 -0300

    feat: add statuses enums

---
+++ validator-backend/src/enums.ts
@@ -0,0 +1,13 @@
+export const Status = {
+  VALID: 0,
+  INVALID: 1,
+  CANCELLED: 2,
+  NULL: 3,
+}
+
+export const MecConformityStatus = {
+  VALID: 0,
+  INVALID: 1,
+  INDETERMINATE: 2,
+  NULL: 3,
+}
