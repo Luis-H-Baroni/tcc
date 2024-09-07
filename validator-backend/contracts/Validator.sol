d90116b3156b89a56e9eea31cca6d872ef3c2027
Luis Baroni <baroniluishenrique@gmail.com>
2024-09-07 10:31:05 -0300

    refactor: adds MEC conformity status to Register struct. changes statuses to enums. updates methods and add events

--- validator-backend/contracts/Validator.sol
+++ validator-backend/contracts/Validator.sol
@@ -1,19 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "hardhat/console.sol";
-
 contract Validator {
     struct Register {
         bytes32 hash;
         bytes32 publicKey;
         bool exists;
-        bool valid;
+        Status status;
+        MecConformityStatus mecConformityStatus;
     }
 
     // documentHash => Register
     mapping (bytes32 => Register[]) registers;    
 
+    // VALID:0, INVALID:1, CANCELLED:2, NULL:3
+    enum Status { VALID, INVALID, CANCELLED, NULL}
+    // VALID:0, INVALID:1, INDETERMINATE:2, NULL:3
+    enum MecConformityStatus { VALID, INVALID, INDETERMINATE, NULL }
    
     function checkRegisterOwner(bytes32 documentHash, bytes32 publicKey) public view returns (bool) {
         Register[] memory register = registers[documentHash];
@@ -25,30 +28,45 @@ contract Validator {
         return false;
     }
 
-    function storeHash(bytes32 documentHash, bytes32 publicKey) public {
-        
-        console.logBytes32(publicKey);
-        console.logBytes32(documentHash);
-
+    function storeHash(bytes32 documentHash, bytes32 publicKey, MecConformityStatus mecConformityStatus) public {
         bool alreadyRegistered = checkRegisterOwner(documentHash, publicKey);
         require(!alreadyRegistered, "Document already registered for this public key");
         
-
-        registers[documentHash].push(Register(documentHash, publicKey, true, true));
+        registers[documentHash].push(Register(documentHash, publicKey, true, Status.VALID, mecConformityStatus));
     }
 
     function verifyHash(bytes32 documentHash) public view returns (Register[] memory) {
         return registers[documentHash];
     }
 
-    function updateStatus(bytes32 documentHash, bytes32 publicKey, bool status) public {
-        console.logBool(status);
+    function updateStatus(
+    bytes32 documentHash, 
+    bytes32 publicKey, 
+    Status status, 
+    MecConformityStatus mecConformityStatus
+    ) public {
         Register[] storage register = registers[documentHash];
         for (uint i = 0; i < register.length; i++) {
             if (register[i].publicKey == publicKey) {
-                register[i].valid = status;
+                if (status != Status.NULL && register[i].status != status) {
+                    register[i].status = status;
+                    emit StatusUpdated(documentHash, publicKey, status);
+                }
+                
+                if (mecConformityStatus != MecConformityStatus.NULL && register[i].mecConformityStatus != mecConformityStatus) {
+                    register[i].mecConformityStatus = mecConformityStatus;
+                    emit MecConformityStatusUpdated(documentHash, publicKey, mecConformityStatus);
+                }
+
+                break;  
             }
         }
     }
+
+
+    event StatusUpdated(bytes32 indexed documentHash, bytes32 indexed publicKey, Status status);
+    event MecConformityStatusUpdated(bytes32 indexed documentHash, bytes32 indexed publicKey, MecConformityStatus mecConformityStatus);
+
+   
 
 }
