43d2bf391658040fcc813fcdc477d174c3e81d41
Luis Baroni <baroniluishenrique@gmail.com>
2024-09-07 10:32:51 -0300

    feat: adds provider url, contract info and wallet key as environment variables

--- validator-backend/src/config/configuration.ts
+++ validator-backend/src/config/configuration.ts
@@ -1,5 +1,12 @@
 export default () => ({
   contract: {
     methods: process.env.CONTRACT_METHODS.split(','),
+    address: process.env.CONTRACT_ADDRESS,
+  },
+  provider: {
+    rpcProviderUrl: process.env.RPC_PROVIDER_URL,
+  },
+  wallet: {
+    privateKey: process.env.WALLET_PRIVATE_KEY,
   },
 })
--- validator-backend/src/ethers.module.ts
+++ validator-backend/src/ethers.module.ts
@@ -1,38 +1,44 @@
 import { Module, Global } from '@nestjs/common'
 import { ethers } from 'ethers'
 import contractArtifact from '../artifacts/contracts/Validator.sol/Validator.json'
+import { ConfigService } from '@nestjs/config'
 
 @Global()
 @Module({
   providers: [
     {
       provide: 'ETHERS_PROVIDER',
-      useFactory: () => {
-        return new ethers.JsonRpcProvider(
-          'https://rpc-amoy.polygon.technology/',
-        )
+      useFactory: (configService: ConfigService) => {
+        const url = configService.get('provider.rpcProviderUrl')
+        return new ethers.JsonRpcProvider(url)
       },
+      inject: [ConfigService],
     },
     {
       provide: 'ETHERS_WALLET',
-      useFactory: (provider: ethers.JsonRpcProvider) => {
-        return new ethers.Wallet(
-          '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
-          provider,
-        )
+      useFactory: (
+        provider: ethers.JsonRpcProvider,
+        configService: ConfigService,
+      ) => {
+        const privateKey = configService.get('wallet.privateKey')
+        return new ethers.Wallet(privateKey, provider)
       },
-      inject: ['ETHERS_PROVIDER'],
+      inject: ['ETHERS_PROVIDER', ConfigService],
     },
     {
       provide: 'ETHERS_CONTRACT',
-      useFactory: (provider: ethers.JsonRpcProvider) => {
+      useFactory: (
+        provider: ethers.JsonRpcProvider,
+        configService: ConfigService,
+      ) => {
+        const contractAddress = configService.get('contract.address')
         return new ethers.Contract(
-          '0x9FE216ACEc9Af5b5617626eBe3BC0a8393347c86',
+          contractAddress,
           contractArtifact.abi,
           provider,
         )
       },
-      inject: ['ETHERS_PROVIDER'],
+      inject: ['ETHERS_PROVIDER', ConfigService],
     },
   ],
   exports: ['ETHERS_PROVIDER', 'ETHERS_WALLET', 'ETHERS_CONTRACT'],
